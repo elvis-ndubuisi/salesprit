74344aadbf916e0de57693095b0dd4e2c73ed0a5
elvis-ndubuisi <provictor.ie@gmail.com>
2023-04-10 13:10:44 +0100

    Redesigned AuthLayout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,17 +1,63 @@
 import { Outlet } from "react-router-dom"
 
-// import illustration from "../assets/Illustration.svg"
+import illustration from "../assets/illustrations/Illustration.svg"
+import line from "../assets/patterns/line.svg"
+import vector from "../assets/patterns/vector.svg"
+import vector1 from "../assets/patterns/vector-1.svg"
+import vector2 from "../assets/patterns/vector-2.svg"
+import vector3 from "../assets/patterns/vector-3.svg"
+import bubbles from "../assets/patterns/bubble-pattern.svg"
 
+// TODO: check max width for illustration
+// TODO: fix vector images that are not displaying
 export default function AuthLayout() {
     return (
-        <section className="w-full h-full flex items-center justify-center">
-            <section className="h-full flex-1 bg-red-300">
-                <img src={""} alt="cartoon lady holding a mobile phone" />
+        <section className="w-full h-full flex items-center justify-center bg-sl-light relative">
+            <section className="h-full flex-1 grid items-center justify-center">
+                <img
+                    src={illustration}
+                    alt="cartoon lady holding a mobile phone"
+                    className="max-w-sm object-contain"
+                />
             </section>
-            <div></div>
-            <section className="h-full flex-1 bg-red-300">
+            <img src={line} alt="divider" className="max-h-[80%]" />
+            <section className="h-full flex-1 grid items-center justify-center">
                 <Outlet />
             </section>
+
+            {/* Patterns */}
+            <img
+                src={vector1}
+                alt="pattern top left"
+                className="absolute top-0 left-0"
+            />
+            <img
+                src={vector}
+                alt="pattern top almost centered"
+                className="absolute top-0 right-1/2"
+            />
+            <img
+                src={vector2}
+                alt="pattern bottom right"
+                className="absolute bottom-0 right-0"
+            />
+            <img
+                src={vector3}
+                alt="pattern bottom right"
+                className="absolute bottom-0 -right-0"
+            />
+
+            {/* Bubbles */}
+            <img
+                src={bubbles}
+                alt="bubbles top right"
+                className="absolute -top-10 right-[10%]"
+            />
+            <img
+                src={bubbles}
+                alt="bubbles bottom center"
+                className="absolute bottom-0 right-1/2"
+            />
         </section>
     )
 }
