d3758b0176674d2542cafa620079ab57fffcd281
elvis-ndubuisi <provictor.ie@gmail.com>
2023-04-11 14:02:51 +0100

    Updated - main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import "./index.css"
+import AppLayout from "./layouts/AppLayout"
+import PosLayout from "./layouts/PosLayout"
+import AuthLayout from "./layouts/AuthLayout"
+import Splash from "./screens/Splash"
+import Login from "./screens/Login"
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+        children: [
+            {
+                element: <PosLayout />,
+                // loader: async (params: any) => {
+                //     console.log(params)
+                // },
+                children: [{ path: "pos", element: <Splash /> }],
+            },
+            {
+                element: <AuthLayout />,
+                children: [
+                    { path: "login", element: <Login /> },
+                    { path: "logout", element: <div>log out elem</div> },
+                ],
+            },
+            { path: "splash", element: <Splash /> },
+        ],
+    },
+])
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 )
