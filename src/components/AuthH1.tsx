3ea0520ada5d46bbada701a6b1855aa5da12c249
elvis-ndubuisi <provictor.ie@gmail.com>
2023-04-11 13:50:03 +0100

    Removed AuthH1 - moved styles to index.css

--- src/components/AuthH1.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from "react"
-
-// TODO: check font-size
-// TODO: check margin top and down
-export default function AuthH1({ children }: PropsWithChildren) {
-    return (
-        <h1 className="font-bold capitalize text-[2.75rem] my-2">{children}</h1>
-    )
-}
