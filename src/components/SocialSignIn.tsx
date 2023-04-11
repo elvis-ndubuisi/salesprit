a27e761d2bd50e50aee07e27dda1c12d92f9e37f
elvis-ndubuisi <provictor.ie@gmail.com>
2023-04-11 14:01:07 +0100

    Renamed component: SocialSignin to SocialSignIn

---
+++ src/components/SocialSignIn.tsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom"
+
+import SocialButton from "./SocialButton"
+import { GoogleIcon, FacebookIcon } from "./Icons"
+
+export default function SocialSignIn() {
+    return (
+        <div>
+            <div className="text-sl-grey w-full font-medium my-8 flex items-center justify-between gap-3 before:content-[' '] after:content-[' '] before:h-[2px] after:h-[2px] before:flex-1 after:flex-1 before:bg-sl-stroke after:bg-sl-stroke">
+                Or
+            </div>
+            <div className="flex items-center gap-5 justify-center">
+                <SocialButton socialName="goggle">
+                    <GoogleIcon />
+                </SocialButton>
+                <SocialButton socialName="facebook">
+                    <FacebookIcon />
+                </SocialButton>
+            </div>
+
+            <p className="font-medium text-[1.3rem]">
+                Don&apos;t have an account?{" "}
+                <Link to="register" className="clickable-button">
+                    Sign Up
+                </Link>
+            </p>
+        </div>
+    )
+}
