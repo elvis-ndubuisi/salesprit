d7377628a1c4b56667f4b2bfcdb914a612aa03a2
elvis-ndubuisi <provictor.ie@gmail.com>
2023-04-11 14:01:30 +0100

    Styles update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
         extend: {
             colors: {
                 "sl-primary": "#F67F20",
-                "sl-secondary": "003048",
+                "sl-secondary": "#003048",
                 "sl-light": "#FEF2E9",
                 "sl-stroke": "#E5E5E5",
                 "sl-grey": "#777777",
