3d8e528dc507c0706d88bb3366c662771961d4e8
elvis-ndubuisi <provictor.ie@gmail.com>
2023-04-12 14:50:48 +0100

    Main screen layout

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,3 +1,20 @@
+import CashierProfile from "../components/CashierProfile"
+import MainTopBar from "../components/MainTopBar"
+
 export default function Main() {
-    return <div>main</div>
+    return (
+        <>
+            <section className="pt-5 border-r-2 border-sl-grey px-5">
+                <MainTopBar />
+
+                <h2 className="auth-heading">special menu for you</h2>
+
+                <section>product grid</section>
+            </section>
+
+            <section className="mt-5">
+                <CashierProfile />
+            </section>
+        </>
+    )
 }
