39bce778ab28938ef7945060f35fac99371071dd
elvis-ndubuisi <provictor.ie@gmail.com>
2023-04-13 13:50:26 +0100

    Dashboard screen layout

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,3 +1,93 @@
+import { GoKebabVertical } from "react-icons/go"
+
 export default function Dashboard() {
-    return <div>dashboard</div>
+    return (
+        <section className="col-span-2 h-screen px-5">
+            <h2 className="section-heading h-[5%]">manager dashboard</h2>
+
+            <section className="h-[95%] w-full py-4 grid grid-cols-3 gap-3">
+                <section className="p-3 rounded-lg shadow-xl">
+                    <div className="flex items-center justify-between">
+                        <h3 className="capitalize font-semibold text-2xl text-sl-secondary">
+                            total income
+                        </h3>
+
+                        <button type="button">
+                            <GoKebabVertical size={20} />
+                        </button>
+                    </div>
+                </section>
+
+                <section className="p-3 rounded-lg shadow-xl">
+                    <div className="flex items-center justify-between">
+                        <h3 className="capitalize font-semibold text-2xl text-sl-secondary">
+                            monthly income
+                        </h3>
+
+                        <button type="button">
+                            <GoKebabVertical size={20} />
+                        </button>
+                    </div>
+                </section>
+
+                <section className="p-3 rounded-lg shadow-xl">
+                    <div className="flex items-center justify-between">
+                        <h3 className="capitalize font-semibold text-2xl text-sl-secondary">
+                            tax income
+                        </h3>
+
+                        <button type="button">
+                            <GoKebabVertical size={20} />
+                        </button>
+                    </div>
+                </section>
+
+                <section className="p-3 rounded-lg shadow-xl col-span-2">
+                    <p>dashboard</p>
+                </section>
+
+                <section className="p-3 rounded-lg shadow-xl">
+                    <div className="flex items-center justify-between">
+                        <h3 className="capitalize font-semibold text-2xl text-sl-secondary">
+                            best dished
+                        </h3>
+
+                        <button type="button">
+                            <GoKebabVertical size={20} />
+                        </button>
+                    </div>
+
+                    <table className="w-full">
+                        <thead>
+                            <td className="py-3 text-sl-grey font-bold text-base capitalize">
+                                dishes
+                            </td>
+                            <td className="py-3 text-sl-grey font-bold text-base capitalize text-right">
+                                orders
+                            </td>
+                        </thead>
+
+                        <tr>
+                            <td className="flex gap-2">
+                                <img
+                                    src=""
+                                    alt="food"
+                                    className="w-11 h-11 bg-sl-grey object-cover rounded overflow-hidden"
+                                />
+                                <div className="flex flex-col">
+                                    <p className="font-medium text-sl-secondary">
+                                        japanies food
+                                    </p>
+                                    <p className="text-sl-primary font-medium">
+                                        $800
+                                    </p>
+                                </div>
+                            </td>
+                            <td className="text-right">200</td>
+                        </tr>
+                    </table>
+                </section>
+            </section>
+        </section>
+    )
 }
